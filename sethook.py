6053fde3118f21f96d5b9da7b013e686546e8278
suhov <suhovaa@desnol.ru>
2018-07-04 11:52:20 +0300

    add first tests

---
+++ sethook.py
@@ -0,0 +1,10 @@
+from viberbot import Api
+from viberbot.api.bot_configuration import BotConfiguration
+auth_token_out = '4807270b7ee7d14d-fa37d43de286a0ef-be81bbab61de274b'
+viber = Api(BotConfiguration(
+    name='Itilium-bot',
+    avatar='http://site.com/avatar.jpg',
+    auth_token=auth_token_out
+))
+viber.unset_webhook()
+viber.set_webhook("https://botitilium.herokuapp.com/")
