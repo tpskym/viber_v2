3778da7e0c834e9d905b94220fda32570efb0ec4
suhov <suhovaa@desnol.ru>
2018-07-04 12:33:21 +0300

    add first tests

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python viber.py
+web: gunicorn -b 0.0.0.0:$PORT app:app
--- viber.py
+++ viber.py
@@ -1,4 +1,4 @@
-
+import os
 from viberbot import Api
 from viberbot.api.bot_configuration import BotConfiguration
 from viberbot.api.messages import VideoMessage
@@ -1463,4 +1463,5 @@ def tests():
 # tests()
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
