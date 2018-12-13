07371ca6897fffdf7034ce6fd4e6b421ff427464
suhovaa <suhovaa@desnol.ru>
2018-12-13 13:04:13 +0300

    1

--- viber.py
+++ viber.py
@@ -44,6 +44,35 @@ import json
 # Close communication with the database
 #cur.close()
 #conn.close()
+
+def SaveState():
+    DATABASE_URL = os.environ['DATABASE_URL']
+
+    # Connect to an existing database
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+    try:
+        cur.execute("select * from information_schema.tables where table_name=%s", ('data_users',))
+        if(cur.rowcount == 0):
+            # Execute a command: this creates a new table
+            cur.execute("CREATE TABLE data_users (id serial PRIMARY KEY, sender_id varchar(50), state_id varchar(36), carousel_id varchar(36),data_user text, data text );")
+
+        # Pass data to fill a query placeholders and let Psycopg perform
+        # the correct conversion (no more SQL injections!)
+        cur.execute("INSERT INTO data_users (sender_id, state_id, carousel_id, data_user, data) VALUES (%s, %s, %s, %s, %s)",
+              ("sender_id_test", "state_id_test", "carousel_id_test", "data_user", "data"))
+
+        # Make the changes to the database persistent
+        conn.commit()
+
+        # Close communication with the database
+    finally:
+        cur.close()
+        conn.close()
+
+def RestoreState():
+
 app = Flask(__name__)
 @app.route('/',  methods=['GET'])
 def incomingGET():
@@ -71,16 +100,26 @@ def incomingGET():
 
         cur.execute("INSERT INTO data_users (sender_id, state_id, carousel_id, data_user, data) VALUES (%s, %s, %s, %s, %s)",
               ("sender_id_test", "state_id_test", "carousel_id_test", "data_user", "data"))
-
+        text += '\n' + "Add string"
         # Query the database and obtain data as Python objects
         cur.execute("SELECT sender_id,  state_id,  carousel_id, data_user,  data FROM data_users;")
         sender_id, state_id, carousel_id, data_user, data  = cur.fetchone()
-        text += "\nsender_id: " + sender_id
-        text += "\nstate_id: " + state_id
-        text += "\ncarousel_id: " + carousel_id
-        text += "\ndata_user: " + data_user
-        text += "\ndata: " + data
+        text += '\n' + "sender_id: " + sender_id
+        text += '\n' + "state_id: " + state_id
+        text += '\n' + "carousel_id: " + carousel_id
+        text += '\n' + "data_user: " + data_user
+        text += '\n' + "data: " + data
 
+        cur.execute("DELETE FROM data_users WHERE sender_id = %s", "sender_id_test");
+        text += '\n' + "remove string"
+
+        cur.execute("SELECT sender_id,  state_id,  carousel_id, data_user,  data FROM data_users;")
+        sender_id, state_id, carousel_id, data_user, data  = cur.fetchone()
+        text += '\n' + "sender_id: " + sender_id
+        text += '\n' + "state_id: " + state_id
+        text += '\n' + "carousel_id: " + carousel_id
+        text += '\n' + "data_user: " + data_user
+        text += '\n' + "data: " + data
 
         # Make the changes to the database persistent
         conn.commit()
