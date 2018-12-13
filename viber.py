cbe066c14d37940a59dde7805583be5b1482252d
suhovaa <suhovaa@desnol.ru>
2018-12-13 17:18:19 +0300

    1

--- viber.py
+++ viber.py
@@ -45,97 +45,147 @@ import json
 #cur.close()
 #conn.close()
 
-def SaveState():
-    DATABASE_URL = os.environ['DATABASE_URL']
-
-    # Connect to an existing database
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-    # Open a cursor to perform database operations
-    cur = conn.cursor()
+def SaveStateToPostgress(sender_id, state_id, carousel_id, data_user, data):
+    data_user_string = json.dumps(data_user)
+    data_bot_string = json.dumps(data)
+    state = True
     try:
+        DATABASE_URL = os.environ['DATABASE_URL']
+        # Connect to an existing database
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        # Open a cursor to perform database operations
+
+        cur = conn.cursor()
+
+        need_drop = True
         cur.execute("select * from information_schema.tables where table_name=%s", ('data_users',))
         if(cur.rowcount == 0):
             # Execute a command: this creates a new table
             cur.execute("CREATE TABLE data_users (id serial PRIMARY KEY, sender_id varchar(50), state_id varchar(36), carousel_id varchar(36),data_user text, data text );")
+            need_drop = False
 
+        if need_drop:
+            cur.execute("DELETE FROM data_users WHERE sender_id = %s", (sender_id,));
         # Pass data to fill a query placeholders and let Psycopg perform
         # the correct conversion (no more SQL injections!)
         cur.execute("INSERT INTO data_users (sender_id, state_id, carousel_id, data_user, data) VALUES (%s, %s, %s, %s, %s)",
-              ("sender_id_test", "state_id_test", "carousel_id_test", "data_user", "data"))
+              (sender_id, state_id, carousel_id, data_user_string, data_bot_string))
 
         # Make the changes to the database persistent
         conn.commit()
-
         # Close communication with the database
+    except:
+        state = False
     finally:
         cur.close()
         conn.close()
+    return state
 
-def RestoreState():
-    pass
+def RestoreStateFromPostgress(sender_id):
+    state = False
+    is_error = False
+    try:
+        DATABASE_URL = os.environ['DATABASE_URL']
+        # Connect to an existing database
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        # Open a cursor to perform database operations
+        cur = conn.cursor()
+
+        cur.execute("select * from information_schema.tables where table_name=%s", ('data_users',))
+        if(cur.rowcount == 0):
+            # Execute a command: this creates a new table
+            cur.execute("CREATE TABLE data_users (id serial PRIMARY KEY, sender_id varchar(50), state_id varchar(36), carousel_id varchar(36),data_user text, data text );")
+            conn.commit()
+            restore_ok = False
+
+        if not restore_ok:
+            return {'error': is_error, 'state' : False}
+        cur.execute("SELECT sender_id,  state_id,  carousel_id, data_user,  data FROM data_users WHERE sender_id = %s", (sender_id,))
+        result_query = cur.fetchone()
+        if(not result_query == None):
+            return {'state':True, 'sender_id':result_query[0], 'state_id':result_query[1], 'carousel_id':result_query[2],'data_user': json.loads(result_query[3]), 'data':json.loads(result_query[4])}
+        else:
+            return {'error': is_error, 'state' : False}
+
+        # Make the changes to the database persistent
+
+        # Close communication with the database
+    except:
+        is_error = True
+        restore_ok = False
+    finally:
+        cur.close()
+        conn.close()
+    return {'error': is_error, 'state' : state}
+
+
 
 app = Flask(__name__)
 @app.route('/',  methods=['GET'])
 def incomingGET():
-    DATABASE_URL = os.environ['DATABASE_URL']
-    text = "empty"
-    # Connect to an existing database
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-    # Open a cursor to perform database operations
-    cur = conn.cursor()
-    try:
-        cur.execute("select * from information_schema.tables where table_name=%s", ('data_users',))
-        if(cur.rowcount > 0):
-            text = "exist_table"
-        else:
-            text = "NOT exist_table"
-            # Execute a command: this creates a new table
-            cur.execute("CREATE TABLE data_users (id serial PRIMARY KEY, sender_id varchar(50), state_id varchar(36), carousel_id varchar(36),data_user text, data text );")
-            text += "\nTable created"
-
-
-
-        # Pass data to fill a query placeholders and let Psycopg perform
-        # the correct conversion (no more SQL injections!)
-
-        cur.execute("INSERT INTO data_users (sender_id, state_id, carousel_id, data_user, data) VALUES (%s, %s, %s, %s, %s)",
-              ("sender_id_test", "state_id_test", "carousel_id_test", "data_user", "data"))
-        text += '\n' + "Add string"
-        # Query the database and obtain data as Python objects
-        cur.execute("SELECT sender_id,  state_id,  carousel_id, data_user,  data FROM data_users;")
-        result_query = cur.fetchone()
-        if(not result_query == None):
-            text += '\n' + "sender_id: " + result_query[0]
-            text += '\n' + "state_id: " + result_query[1]
-            text += '\n' + "carousel_id: " + result_query[2]
-            text += '\n' + "data_user: " + result_query[3]
-            text += '\n' + "data: " + result_query[4]
-        else:
-            text += '\n' + "empty result"
-
-
-        cur.execute("DELETE FROM data_users WHERE sender_id = %s", ("sender_id_test",));
-        text += '\n' + "remove string"
-
-        cur.execute("SELECT sender_id,  state_id,  carousel_id, data_user,  data FROM data_users;")
-        result_query = cur.fetchone()
-        if(not result_query == None):
-            text += '\n' + "sender_id: " + result_query[0]
-            text += '\n' + "state_id: " + result_query[1]
-            text += '\n' + "carousel_id: " + result_query[2]
-            text += '\n' + "data_user: " + result_query[3]
-            text += '\n' + "data: " + result_query[4]
-        else:
-            text += '\n' + "empty result"
-
-        # Make the changes to the database persistent
-        conn.commit()
-
-        # Close communication with the database
-    finally:
-        cur.close()
-        conn.close()
+    text = __name__
+    # DATABASE_URL = os.environ['DATABASE_URL']
+    # text = "empty"
+    # # Connect to an existing database
+    # conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    #
+    #
+    #
+    #
+    # # Open a cursor to perform database operations
+    # cur = conn.cursor()
+    # try:
+    #     cur.execute("select * from information_schema.tables where table_name=%s", ('data_users',))
+    #     if(cur.rowcount > 0):
+    #         text = "exist_table"
+    #     else:
+    #         text = "NOT exist_table"
+    #         # Execute a command: this creates a new table
+    #         cur.execute("CREATE TABLE data_users (id serial PRIMARY KEY, sender_id varchar(50), state_id varchar(36), carousel_id varchar(36),data_user text, data text );")
+    #         text += "\nTable created"
+    #
+    #
+    #
+    #     # Pass data to fill a query placeholders and let Psycopg perform
+    #     # the correct conversion (no more SQL injections!)
+    #
+    #     cur.execute("INSERT INTO data_users (sender_id, state_id, carousel_id, data_user, data) VALUES (%s, %s, %s, %s, %s)",
+    #           ("sender_id_test", "state_id_test", "carousel_id_test", "data_user", "data"))
+    #     text += '\n' + "Add string"
+    #     # Query the database and obtain data as Python objects
+    #     cur.execute("SELECT sender_id,  state_id,  carousel_id, data_user,  data FROM data_users;")
+    #     result_query = cur.fetchone()
+    #     if(not result_query == None):
+    #         text += '\n' + "sender_id: " + result_query[0]
+    #         text += '\n' + "state_id: " + result_query[1]
+    #         text += '\n' + "carousel_id: " + result_query[2]
+    #         text += '\n' + "data_user: " + result_query[3]
+    #         text += '\n' + "data: " + result_query[4]
+    #     else:
+    #         text += '\n' + "empty result"
+    #
+    #
+    #     cur.execute("DELETE FROM data_users WHERE sender_id = %s", ("sender_id_test",));
+    #     text += '\n' + "remove string"
+    #
+    #     cur.execute("SELECT sender_id,  state_id,  carousel_id, data_user,  data FROM data_users;")
+    #     result_query = cur.fetchone()
+    #     if(not result_query == None):
+    #         text += '\n' + "sender_id: " + result_query[0]
+    #         text += '\n' + "state_id: " + result_query[1]
+    #         text += '\n' + "carousel_id: " + result_query[2]
+    #         text += '\n' + "data_user: " + result_query[3]
+    #         text += '\n' + "data: " + result_query[4]
+    #     else:
+    #         text += '\n' + "empty result"
+    #
+    #     # Make the changes to the database persistent
+    #     conn.commit()
+    #
+    #     # Close communication with the database
+    # finally:
+    #     cur.close()
+    #     conn.close()
     return text
 
 @app.route('/',  methods=['POST'])
