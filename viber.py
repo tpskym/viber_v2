52884c31a0e50b6eabaf37d5bb4b70306c0fc142
tpskym <alexskyman@gmail.com>
2018-12-13 10:50:18 +0300

    Update viber.py

--- viber.py
+++ viber.py
@@ -46,8 +46,35 @@ import json
 #conn.close()
 app = Flask(__name__)
 @app.route('/',  methods=['GET'])
-def incomingGET():
-    return "Hello, World!"
+def incomingGET():    
+    DATABASE_URL = os.environ['DATABASE_URL']
+    text = DATABASE_URL
+    # Connect to an existing database
+    #conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+    # Open a cursor to perform database operations
+    #cur = conn.cursor()
+
+    # Execute a command: this creates a new table
+    #cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+
+    # Pass data to fill a query placeholders and let Psycopg perform
+    # the correct conversion (no more SQL injections!)
+    #cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",
+    #...      (100, "abc'def"))
+
+    # Query the database and obtain data as Python objects
+    #cur.execute("SELECT * FROM test;")
+    #cur.fetchone()
+    #(1, 100, "abc'def")
+
+    # Make the changes to the database persistent
+    #conn.commit()
+
+    # Close communication with the database
+    #cur.close()
+    #conn.close()
+    return text
     
 @app.route('/',  methods=['POST'])
 def incoming():
