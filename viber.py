09485021f6e468746be266d466d4a4ffb655c646
suhovaa <suhovaa@desnol.ru>
2019-01-11 16:45:22 +0300

    dsf

--- viber.py
+++ viber.py
@@ -2390,5 +2390,10 @@ def incoming():
         viber.send_messages(viber_request.sender.id, [TextMessage(text="Добрый день. Вы подписались на бота Итилиум")])
     elif isinstance(viber_request, ViberDeliveredRequest):
         print(viber_request)
-        #print("Доставлено сообщение  " + viber_request.message_token + " " + " для пользователя " + viber_request.user_id  )
+        try:
+            print("Доставлено сообщение  " + viber_request.message_token + " " + " для пользователя " + viber_request.user_id  )
+        except Exception as e:
+            print ("Error:"+ e.args[0])
+
+
     return Response(status=200)
