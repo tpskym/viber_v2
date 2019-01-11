b27277519a21f4c83e3c7ca3c7eebfb2953b4d0e
suhovaa <suhovaa@desnol.ru>
2019-01-11 16:27:41 +0300

    в

--- viber.py
+++ viber.py
@@ -2385,6 +2385,6 @@ def incoming():
         viber.send_messages(viber_request.sender.id, TextMessage(text="Сообщение не отправлено"))
     elif isinstance(viber_request, ViberConversationStartedRequest) :
         viber.send_messages(viber_request.sender.id, [TextMessage(text="Добрый день. Вы подписались на бота Итилиум")])
-    elif isinstance(viber_request, ViberDeliveredRequest)
+    elif isinstance(viber_request, ViberDeliveredRequest):
         print("Доставлено сообщение  " + viber_request.message_token + " " + " для пользователя " + viber_request.user_id  )
     return Response(status=200)
